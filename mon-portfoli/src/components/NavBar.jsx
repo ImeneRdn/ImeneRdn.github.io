93d2fb87f114ab4416ba905d37114928a581b352
ImeneRdn <imene.ramdan@live.fr>
2023-05-24 21:31:23 +0200

    navbar

---
+++ mon-portfoli/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <h1>Mon portfolio</h1>
+      <ul>
+        <li>Mes projets</li>
+        <li>Mon parcours</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
